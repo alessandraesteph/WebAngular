0239c87f42821b0ec48e478f6aefc7c5a15a13df
Alessandra Estephany <alessandraestph@gmail.com>
2025-02-12 18:27:08 -0300

    Conclusão FrontEnd Login-Page

--- login-page/src/app/app.routes.ts
+++ login-page/src/app/app.routes.ts
@@ -1,16 +1,15 @@
 import { PathLocationStrategy } from '@angular/common';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
     {
-        path: '',
-        redirectTo: 'login',
-        pathMatch: 'full'
-        
-    },
-    {
         path:"login",
         component: LoginComponent
+    },
+    {
+        path:"signup",
+        component: SignupComponent
     }
 ];
